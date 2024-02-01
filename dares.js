03478136d84a6a55372c025fbcdb0d390d11a339
Nabeel Ali <mail2nabeelali@gmail.com>
2024-02-01 10:39:31 +0500

    first commit

---
+++ dares.js
@@ -0,0 +1,414 @@
+window.dares = window.dares || [];
+
+const set_1 = [
+    {
+        "screenTitleLayout": "Row",
+        "question": "What is my favorite drink?",
+        "options": [
+            { "option": "Tea" },
+            { "option": "Coffee" },
+            { "option": "Juice" },
+            { "option": "Milk" }
+        ]
+    },
+    {
+        "screenTitleLayout": "Row",
+        "question": "What is my most favorite color?",
+        "options": [
+            { "option": "Black" },
+            { "option": "White" },
+            { "option": "Red" },
+            { "option": "Brown" }
+        ]
+    },
+    {
+        "screenTitleLayout": "Row",
+        "question": "What I prefer to do in my spare time?",
+        "options": [
+            { "option": "Reading" },
+            { "option": "Using Phone" },
+            { "option": "Sleep" },
+            { "option": "Watching Movies" }
+        ]
+    },
+    {
+        "screenTitleLayout": "Row",
+        "question": "What genre of movies I like?",
+        "options": [
+            { "option": "Comedy" },
+            { "option": "Fiction" },
+            { "option": "Horror" },
+            { "option": "Romantic" }
+        ]
+    },
+    {
+        "screenTitleLayout": "Row",
+        "question": "What is my favorite mean to travel?",
+        "options": [
+            { "option": "Bus" },
+            { "option": "Bike" },
+            { "option": "Cycle" },
+            { "option": "Train" }
+        ]
+    },
+    {
+        "screenTitleLayout": "Row",
+        "question": "Which food I like more?",
+        "options": [
+            { "option": "Pizza" },
+            { "option": "Burger" }
+        ]
+    },
+    {
+        "screenTitleLayout": "Row",
+        "question": "What is my favorite ice cream flavor?",
+        "options": [
+            { "option": "Vanilla" },
+            { "option": "Mango" },
+            { "option": "Chocolate" },
+            { "option": "Strawberry" }
+        ]
+    },
+    {
+        "screenTitleLayout": "Row",
+        "question": "What is more important to me?",
+        "options": [
+            { "option": "Power" },
+            { "option": "Money" },
+            { "option": "Love" }
+        ]
+    },
+    {
+        "screenTitleLayout": "Row",
+        "question": "What part of the day do I like the most?",
+        "options": [
+            { "option": "Morning" },
+            { "option": "Afternoon" },
+            { "option": "Evening" },
+            { "option": "Night" }
+        ]
+    },
+    {
+        "screenTitleLayout": "Row",
+        "question": "What power I would prefer to select?",
+        "options": [
+            { "option": "Reading Mind" },
+            { "option": "Being Superfast" },
+            { "option": "Invisible" },
+            { "option": "Fly" }
+        ]
+    }
+]
+
+const set_2 = [
+    {
+        "screenTitleLayout": "Row",
+        "question": "Which type of party do I enjoy the most?",
+        "options": [
+            { "option": "Dance Party" },
+            { "option": "Game Night" },
+            { "option": "Movie Marathon" },
+            { "option": "Outdoor BBQ" }
+        ]
+    },
+    {
+        "screenTitleLayout": "Row",
+        "question": "What's my preferred way to communicate?",
+        "options": [
+            { "option": "Texting" },
+            { "option": "Calling" },
+            { "option": "FaceTime" },
+            { "option": "Memes and Emojis" }
+        ]
+    },
+    {
+        "screenTitleLayout": "Row",
+        "question": "If I were an animal, what would I be?",
+        "options": [
+            { "option": "Loyal Dog" },
+            { "option": "Curious Cat" },
+            { "option": "Playful Dolphin" },
+            { "option": "Wise Owl" }
+        ]
+    },
+    {
+        "screenTitleLayout": "Row",
+        "question": "What's my go-to karaoke song?",
+        "options": [
+            { "option": "Bohemian Rhapsody" },
+            { "option": "Wannabe - Spice Girls" },
+            { "option": "Shake It Off - Taylor Swift" },
+            { "option": "Don't Stop Believin' - Journey" }
+        ]
+    },
+    {
+        "screenTitleLayout": "Row",
+        "question": "How do I like my pizza toppings?",
+        "options": [
+            { "option": "Pepperoni and Mushrooms" },
+            { "option": "Vegetarian Deluxe" },
+            { "option": "Hawaiian with Pineapple" },
+            { "option": "Meat Lovers" }
+        ]
+    },
+    {
+        "screenTitleLayout": "Row",
+        "question": "What's my favorite social media platform?",
+        "options": [
+            { "option": "Instagram" },
+            { "option": "Twitter" },
+            { "option": "Snapchat" },
+            { "option": "Facebook" }
+        ]
+    },
+    {
+        "screenTitleLayout": "Row",
+        "question": "If I could travel anywhere, where would I go first?",
+        "options": [
+            { "option": "Paris, France" },
+            { "option": "Tokyo, Japan" },
+            { "option": "New York City, USA" },
+            { "option": "Bora Bora, French Polynesia" }
+        ]
+    },
+    {
+        "screenTitleLayout": "Row",
+        "question": "What's my signature dance move?",
+        "options": [
+            { "option": "The Sprinkler" },
+            { "option": "The Floss" },
+            { "option": "The Moonwalk" },
+            { "option": "The Robot" }
+        ]
+    },
+    {
+        "screenTitleLayout": "Row",
+        "question": "If I had a superpower, what would it be?",
+        "options": [
+            { "option": "Teleportation" },
+            { "option": "Time Travel" },
+            { "option": "Invisibility" },
+            { "option": "Mind Reading" }
+        ]
+    },
+    {
+        "screenTitleLayout": "Row",
+        "question": "What's my favorite nostalgic TV show?",
+        "options": [
+            { "option": "Friends" },
+            { "option": "The Fresh Prince of Bel-Air" },
+            { "option": "Saved by the Bell" },
+            { "option": "Full House" }
+        ]
+    }
+]
+
+const set_3 = [
+    {
+        "screenTitleLayout": "Row",
+        "question": "If I could have any job for a day, what would it be?",
+        "options": [
+            { "option": "Professional Chef" },
+            { "option": "Movie Director" },
+            { "option": "Secret Agent" },
+            { "option": "Rockstar Musician" }
+        ]
+    },
+    {
+        "screenTitleLayout": "Row",
+        "question": "What's my guilty pleasure snack?",
+        "options": [
+            { "option": "Chocolate-covered Pretzels" },
+            { "option": "Chips and Salsa" },
+            { "option": "Ice Cream with Potato Chips" },
+            { "option": "Gummy Bears" }
+        ]
+    },
+    {
+        "screenTitleLayout": "Row",
+        "question": "Which celebrity do I secretly admire the most?",
+        "options": [
+            { "option": "Ryan Reynolds" },
+            { "option": "Emma Watson" },
+            { "option": "Dwayne Johnson" },
+            { "option": "Jennifer Lawrence" }
+        ]
+    },
+    {
+        "screenTitleLayout": "Row",
+        "question": "What's my ultimate dream vacation destination?",
+        "options": [
+            { "option": "Santorini, Greece" },
+            { "option": "Maui, Hawaii" },
+            { "option": "Swiss Alps, Switzerland" },
+            { "option": "Marrakech, Morocco" }
+        ]
+    },
+    {
+        "screenTitleLayout": "Row",
+        "question": "What nickname do I secretly wish people would call me?",
+        "options": [
+            { "option": "Captain Awesome" },
+            { "option": "Superstar" },
+            { "option": "Mystery Maestro" },
+            { "option": "Zen Master" }
+        ]
+    },
+    {
+        "screenTitleLayout": "Row",
+        "question": "If I were a character in a movie, would I be the hero or the sidekick?",
+        "options": [
+            { "option": "The Hero" },
+            { "option": "The Sidekick" },
+            { "option": "The Antihero" },
+            { "option": "The Comic Relief" }
+        ]
+    },
+    {
+        "screenTitleLayout": "Row",
+        "question": "What's my favorite type of board game or card game?",
+        "options": [
+            { "option": "Monopoly" },
+            { "option": "Codenames" },
+            { "option": "Uno" },
+            { "option": "Settlers of Catan" }
+        ]
+    },
+    {
+        "screenTitleLayout": "Row",
+        "question": "What's my most unusual talent or skill?",
+        "options": [
+            { "option": "Juggling" },
+            { "option": "Beatboxing" },
+            { "option": "Impersonations" },
+            { "option": "Solving Rubik's Cube" }
+        ]
+    },
+    {
+        "screenTitleLayout": "Row",
+        "question": "If I could time travel, would I go to the past or the future?",
+        "options": [
+            { "option": "Past" },
+            { "option": "Future" },
+            { "option": "I'm happy in the present" },
+            { "option": "I'd rather not time travel" }
+        ]
+    },
+    {
+        "screenTitleLayout": "Row",
+        "question": "What's my favorite type of party game?",
+        "options": [
+            { "option": "Charades" },
+            { "option": "Pictionary" },
+            { "option": "Truth or Dare" },
+            { "option": "Never Have I Ever" }
+        ]
+    }
+]
+
+const set_4 = [
+    {
+        "screenTitleLayout": "Row",
+        "question": "What would be my dream superpower?",
+        "options": [
+            { "option": "Teleportation" },
+            { "option": "Telekinesis" },
+            { "option": "Shape-shifting" },
+            { "option": "Time Travel" }
+        ]
+    },
+    {
+        "screenTitleLayout": "Row",
+        "question": "If I could have any animal as a pet, what would it be?",
+        "options": [
+            { "option": "Penguin" },
+            { "option": "Owl" },
+            { "option": "Dolphin" },
+            { "option": "Sloth" }
+        ]
+    },
+    {
+        "screenTitleLayout": "Row",
+        "question": "What's my favorite way to spend a lazy Sunday afternoon?",
+        "options": [
+            { "option": "Reading a Book" },
+            { "option": "Watching Netflix" },
+            { "option": "Cooking or Baking" },
+            { "option": "Taking a Nap" }
+        ]
+    },
+    {
+        "screenTitleLayout": "Row",
+        "question": "If I could switch lives with any fictional character, who would it be?",
+        "options": [
+            { "option": "Harry Potter" },
+            { "option": "Sherlock Holmes" },
+            { "option": "Wonder Woman" },
+            { "option": "Tony Stark (Iron Man)" }
+        ]
+    },
+    {
+        "screenTitleLayout": "Row",
+        "question": "What's my favorite type of outdoor activity?",
+        "options": [
+            { "option": "Hiking" },
+            { "option": "Picnicking" },
+            { "option": "Camping" },
+            { "option": "Beach Day" }
+        ]
+    },
+    {
+        "screenTitleLayout": "Row",
+        "question": "If I were a famous YouTuber, what content would I create?",
+        "options": [
+            { "option": "Vlogs and Travel" },
+            { "option": "Gaming" },
+            { "option": "DIY and Crafts" },
+            { "option": "Cooking and Food Reviews" }
+        ]
+    },
+    {
+        "screenTitleLayout": "Row",
+        "question": "What's my favorite type of music?",
+        "options": [
+            { "option": "Pop" },
+            { "option": "Rock" },
+            { "option": "Hip-hop" },
+            { "option": "Indie" }
+        ]
+    },
+    {
+        "screenTitleLayout": "Row",
+        "question": "If I could have dinner with any historical figure, who would it be?",
+        "options": [
+            { "option": "Albert Einstein" },
+            { "option": "Cleopatra" },
+            { "option": "Leonardo da Vinci" },
+            { "option": "Martin Luther King Jr." }
+        ]
+    },
+    {
+        "screenTitleLayout": "Row",
+        "question": "What's my go-to karaoke song when I'm feeling confident?",
+        "options": [
+            { "option": "Bohemian Rhapsody" },
+            { "option": "Shallow - Lady Gaga" },
+            { "option": "Uptown Funk - Mark Ronson ft. Bruno Mars" },
+            { "option": "Don't Stop Believin' - Journey" }
+        ]
+    },
+    {
+        "screenTitleLayout": "Row",
+        "question": "What's my favorite type of dessert?",
+        "options": [
+            { "option": "Chocolate Cake" },
+            { "option": "Cheesecake" },
+            { "option": "Ice Cream Sundae" },
+            { "option": "Fruit Tart" }
+        ]
+    }
+]
+
+
+
+window.dares.push(set_1, set_2, set_3, set_4);
